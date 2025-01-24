4a43d2306161b4d9881e33fb5618859dbf99add0
miftiaz <miftiazintisami01@gmail.com>
2025-01-24 16:23:55 +0600

    added register and fixed some bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,65 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js';
+import {User} from '../models/user.model.js';
+import {uploadOnCloudinary} from '../utils/cloudinary.js';
+import {ApiResponse} from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message:"OK!"
+    const {fullName, email, userName, password } = req.body
+    console.log(fullName);
+
+    if (
+        [fullName, email, userName, password].some((field) => 
+        field?.trim() === "")
+    ){
+        throw new ApiError(400, "Please fill in all fields")
+    }
+
+    const existedUser = User.findOne({
+        $or: [
+            {email},
+            {userName}
+        ]
     })
+
+    if (existedUser){
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath ){
+        throw new ApiError(400, "Please upload avatar")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(500, "Failed to upload avatar ")
+    }
+
+    User.create({
+        fullName, 
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: userName.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshTokens"
+    )
+
+    if (!createdUser){
+        throw new ApiError(500, "Failed to create user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse (200, createdUser, "User created successfully")
+    )
 })
 
 export {registerUser}
